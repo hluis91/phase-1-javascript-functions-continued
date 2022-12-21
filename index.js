627bd91ac7f3726ae3f09956802d97f8f9fa1a15
Luis Hernandez <lshrnndz6@gmail.com>
2022-12-21 09:40:51 -0800

    commit tree 6880cb3464c8c1af43dd334015d894217911bc31

--- index.js
+++ index.js
@@ -1,1 +1,131 @@
 // code your solution here
+
+function saturdayFun(event = 'roller-skate'){
+  return `This Saturday, I want to ${event}!`
+}
+
+function mondayWork(work = "go to the office"){
+  return `This Monday, I will ${work}.`
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adj="special") {
+      return `You are ${style}${adj}${style}!`
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function saturdayFun(event = 'roller-skate'){
+//     return `This Saturday, I want to ${event}!`
+// }
+
+// const mondayWork = function(work = 'go to the office') {
+//     return `This Monday, I will ${work}.`;
+// }
+
+// let wrapAdjective = function(style="*") {
+//     return function(adj="special") {
+//       return `You are ${style}${adj}${style}!`
+//     }
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
